637da943f15ff80a740f8a2de0a72ebe16035701
huyhoangcao <huyhoangcao@gmail.com>
2024-05-27 17:38:08 +0700

    Update SVGSprite.cpp
    I have downloaded and used your library; it works very well on Windows. However, when building for Android and iOS, there is an error where the SVG file cannot be opened when using the function: nsvgParseFromFile. I switched to using the Cocos2d-x library code to open the file, and it works well on Android and iOS. Additionally, it is not necessary to include the glfw3 library to load SVG files on Android and iOS. Thank you very much, because with your sample code, I was able to load SVG files on Android and iOS

--- SVGSprite.cpp
+++ SVGSprite.cpp
@@ -29,13 +29,24 @@ bool SVGSprite::init(std::string path, float scale, std::string unit, float dpi)
 		unsigned char* img = NULL;
 		int w, h;
 		//--- parse svg from file
+		/*
+		This code encounters an error when building on Android and iOS. The error occurs because the SVG file cannot be opened. It needs to be switched to use the Cocos2d-x code to open and read the SVG file, as shown in the code snippet below
 		image = nsvgParseFromFile(path.c_str(), unit.c_str(), dpi);
 		if (image == NULL) {
 			log("Could not open SVG image.\n");
 			nsvgDeleteRasterizer(rast);
 			nsvgDelete(image);
 			return false;
+		}*/
+
+		cocos2d::Data data = cocos2d::FileUtils::getInstance()->getDataFromFile(path);
+		if (data.isNull()) {
+			cocos2d::log("Failed to open file: %s", path.c_str());
+			return false;
 		}
+		const unsigned char* rawBytes = data.getBytes();
+		char* buffer = reinterpret_cast<char*>(const_cast<unsigned char*>(rawBytes));
+		image = nsvgParse(buffer, unit.c_str(), dpi);
 		//--- init rasterizer
 		w = (int)image->width * scale;
 		h = (int)image->height * scale;
